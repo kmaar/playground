2dabbcb246311369515c0fed937156040e21540d
Kristian Maar Kristensen <kristianmaar@gmail.com>
2019-03-07 11:57:32 +0200

    Add api call

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,28 @@ import { makeBlogPath } from '../utils'
 import { graphql } from 'gatsby'
 import dateformat from 'dateformat'
 
+import axios from 'axios'
+
+const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min
+
+const min = 100000
+const max = 800999
+
+const getInventory = async () => {
+  try {
+    return await axios.get(`https://steamcommunity.com/inventory/76561197960${getRandomArbitrary(min, max)}/730/2?l=english&count=5000`)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+getInventory().then(response => {
+  console.log(response)
+  // setResults(response ? response.data : null)
+})
+
+getInventory()
+
 export default ({ data }) => (
   <div>
     <h1>My Gatsby Blog</h1>
